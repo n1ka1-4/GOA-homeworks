6bc8b79a17286b91dd72effa48a9cd78ea4dc4d8
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2024-10-03 20:30:43 +0400

    first commit

---
+++ group 102/day 1/lesson1.py
@@ -0,0 +1,27 @@
+name = "nika"           #str   (string)
+age = 13                #int   (integer)
+hight = 145.5           #float (ათწილადი)
+surname = "tefnadze"
+
+studys_programming = True    #boolean     snakecase
+plays_basketball = False     #(bool)      camelcase
+
+
+# print(name + " " + surname)
+
+# print(name + " " + str(age))
+
+print(name + " " +surname + " " + str(age) + " " + str(hight))
+
+
+
+
+
+
+
+
+# print(type(age))
+# print(type(hight))
+# print(type(name))
+# print(type(surname))
+# print(type(studys_programming))
