5857dafe77bee8b942f2cca034eb2bdac0f8ffb8
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2024-10-11 19:04:34 +0400

    second commit

---
+++ day2/classwork.py
@@ -0,0 +1,12 @@
+print('nika')
+print('tefnadze')
+print(13)
+
+
+name='nika'
+surname='tefnadze'
+age=13
+print(name)
+print(surname)
+print(age)
+
---
+++ day2/homework.py
@@ -0,0 +1,40 @@
+
+i=0
+while i<20:
+ i=i+1
+ print('programming is fun!')
+
+menu='food'
+box='toy'
+computer='game'
+house='home'
+book='page'
+
+print(menu ,box ,computer ,house ,book)
+
+
+from turtle import *
+from turtle import color
+speed(30)
+
+
+width(20)
+color('blue')
+color()
+forward(20000)
+left(90)
+forward(30000)
+left(90)
+forward(20000)
+left(90)
+forward(30000)
+penup()
+goto(0,30000)
+pendown()
+left(150)
+forward(20000)
+right(120)
+forward(20000)
+
+exitonclick()
+
