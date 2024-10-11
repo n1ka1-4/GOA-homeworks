a1c1e7040793b1214ba84a1a1d71c7bbc4c94269
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2024-10-12 00:53:26 +0400

    second commit

--- day2/homework.py
+++ day2/homework.py
@@ -1,35 +1,34 @@
 
-# i=0
-# while i<20:
-#  i=i+1
-#  print('programming is fun!')
+i=0
+while i<20:
+ i=i+1
+ print('programming is fun!')
 
-# menu='food'
-# box='toy'
-# computer='game'
-# house='home'
-# book='page'
+menu='food'
+box='toy'
+computer='game'
+house='home'
+book='page'
 
-# print(menu ,box ,computer ,house ,book)
+print(menu ,box ,computer ,house ,book)
 from turtle import *
 
-# Setup turtle
 speed(20)
 penup()
 
-# Set the background color
+
 bgcolor("lightblue")
 
-# Color for the towers
+
 color("darkorange")
 begin_fill()
 
-# Left tower (make it longer)
+
 goto(-300, -200)
 pendown()
 forward(100)
 left(90)
-forward(300)  # Taller tower
+forward(300)  
 left(90)
 forward(100)
 left(90)
@@ -37,14 +36,14 @@ forward(300)
 left(90)
 end_fill()
 
-# Right tower (make it longer)
+
 penup()
 goto(150, -200)
 pendown()
 begin_fill()
 forward(100)
 left(90)
-forward(300)  # Taller tower
+forward(300)  
 left(90)
 forward(100)
 left(90)
@@ -52,11 +51,9 @@ forward(300)
 left(90)
 end_fill()
 
-# Color for the wall
 color("darkred")
 begin_fill()
 
-# Wall between the towers
 penup()
 goto(-200, -200)
 pendown()
@@ -70,11 +67,10 @@ forward(150)
 left(90)
 end_fill()
 
-# Battlements with same color as wall (darkgray)
+
 color("darkorange")
 begin_fill()
 
-# Battlement 1 on the wall
 penup()
 goto(-150, -50)
 pendown()
@@ -87,7 +83,7 @@ left(90)
 forward(50)
 left(90)
 
-# Battlement 2 on the wall
+
 penup()
 goto(-75, -50)
 pendown()
@@ -100,7 +96,6 @@ left(90)
 forward(50)
 left(90)
 
-# Battlement 3 on the wall
 penup()
 goto(0, -50)
 pendown()
@@ -113,7 +108,7 @@ left(90)
 forward(50)
 left(90)
 
-# Battlement 4 on the wall
+
 penup()
 goto(75, -50)
 pendown()
@@ -127,11 +122,11 @@ forward(50)
 left(90)
 end_fill()
 
-# Color for the wall
+
 color("darkred")
 begin_fill()
 
-# Wall between the towers
+
 penup()
 goto(-200, -200)
 pendown()
@@ -147,9 +142,9 @@ end_fill()
 
 color('darkorange')
 begin_fill()
-battlement_size = 45  # Smaller size for the battlements
+battlement_size = 45  
 penup()
-goto(-330, 100)  # First battlement on the left tower
+goto(-330, 100)  
 pendown()
 begin_fill()
 forward(battlement_size)
@@ -163,7 +158,7 @@ left(90)
 end_fill()
 
 penup()
-goto(-275, 100)  # Second battlement on the left tower
+goto(-275, 100)  
 pendown()
 begin_fill()
 forward(battlement_size)
@@ -177,7 +172,7 @@ left(90)
 end_fill()
 
 penup()
-goto(-215, 100)  # Third battlement on the left tower
+goto(-215, 100) 
 pendown()
 begin_fill()
 forward(battlement_size)
@@ -190,23 +185,9 @@ forward(battlement_size)
 left(90)
 end_fill()
 
-# Smaller battlements on top of the right tower (4 side by side)
-penup()
-goto(120, 100)  # First battlement on the right tower
-pendown()
-begin_fill()
-forward(battlement_size)
-left(90)
-forward(battlement_size)
-left(90)
-forward(battlement_size)
-left(90)
-forward(battlement_size)
-left(90)
-end_fill()
 
 penup()
-goto(175, 100)  # Second battlement on the right tower
+goto(120, 100) 
 pendown()
 begin_fill()
 forward(battlement_size)
@@ -220,7 +201,21 @@ left(90)
 end_fill()
 
 penup()
-goto(235, 100)  # Third battlement on the right tower
+goto(175, 100)  
+pendown()
+begin_fill()
+forward(battlement_size)
+left(90)
+forward(battlement_size)
+left(90)
+forward(battlement_size)
+left(90)
+forward(battlement_size)
+left(90)
+end_fill()
+
+penup()
+goto(235, 100)  
 pendown()
 begin_fill()
 forward(battlement_size)
@@ -235,11 +230,11 @@ end_fill()
 
 
 
-# Door color changed to skin tone (light beige)
+
 color("lightyellow")
 begin_fill()
 
-# Door in the middle of the wall
+
 penup()
 goto(-50, -200)
 pendown()
@@ -253,8 +248,5 @@ forward(120)
 left(90)
 end_fill()
 
-# Hide the turtle
 
-
-# Hide the turtle
 exitonclick()
