4a426f95a1f1906df3b503dfc4f7c05c4a0aced1
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2025-01-11 12:46:38 +0400

    commit

---
+++ day50/classwork/lesson0.py
@@ -0,0 +1,33 @@
+# IndexError - გვაქ ლისტი რომელიც ლიგრძეა 3 და ჩვენ ვიჩახებთ 3 ან მეტს
+# NameError - როცა ვიჩახებთ სახელს რომელიც არ არსებობს
+# ValueError - როცა მაგ: სტრინგს ვაქცევ int ან float-ად
+# TypeError - როცა მაგ: int ვუმატებთ str
+# SyntaxError - როცა მაგ: if გამოვიყენე და ბოლოში ორწერტილი არ დაუწერე
+# ZeroDivisionError - როცა რამეს ვყობთ ნულზე
+
+
+# 1
+name = "hello"
+
+try:
+    print(username)
+except ValueError:
+    print("username is not defined")
+
+# 2
+list1 = [1,2,3,4]
+
+try:
+    print(list1[4])
+except ValueError:
+    print("list index out of range")
+
+# 3
+
+try:
+    print(int("12.23"))
+except ValueError:
+    print("ValueError: int not containes dot (.)")
+
+
+# try/except - ვიყენებთ დიბაგისთვის მაგ ჩალიან გამოსადეგარია უსერთან მშაობისას
