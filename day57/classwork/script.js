a8fa352b7a193643e2237c238cee80950d079b75
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2025-01-28 18:44:54 +0400

    commit

---
+++ day57/classwork/script.js
@@ -0,0 +1,5 @@
+// we learned String and Number data types
+// DOM is a tree structure of HTML elements we can access the DOM using JavaScript
+
+let hello = document.getElementById('hello');
+console.log(hello);
---
+++ day57/homework/script.js
@@ -0,0 +1,8 @@
+// DOM is a tree structure of HTML elements we can access the DOM using JavaScript
+
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+// console is same as print in python and it is used to print the output of the code in the console of the browser.
+// let keyword is used to declare a variable in javascript.
