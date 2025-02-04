85dc13488dc121ec722b01eb7639b55db0f9fb23
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2025-02-04 15:41:46 +0400

    commit

---
+++ day59/homework/script.js
@@ -0,0 +1,11 @@
+let x = Number(prompt("Enter the first number: "));
+let y = Number(prompt("Enter the second number: "));
+let z = Number(prompt("Enter the third number: "));
+
+let name = prompt("Enter your name: ");
+let username = prompt("Enter your username: ");
+
+console.log(x + y + z);
+console.log("Hello " + name + "/" + username + "!");
+
+// name attribute is used to axcess the value of the input field it is similar to id attribute
