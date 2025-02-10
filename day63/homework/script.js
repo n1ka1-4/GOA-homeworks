8496f8460061d196bddbb694c69dad8979a0de8a
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2025-02-10 21:09:15 +0400

    commit

---
+++ day63/homework/script.js
@@ -0,0 +1,30 @@
+// 1
+let i = 1;
+
+while (i < 200) {
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+    i++;
+}
+
+// 2
+i = 51;
+while (i < 150) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+// 3
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+
+// 4
+for (let i = 100; i < 300; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
