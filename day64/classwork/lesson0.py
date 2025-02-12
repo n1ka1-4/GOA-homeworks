ec1694ca7f9a790094a8af11fd3d5eb8b04ee123
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2025-02-12 22:33:09 +0400

    commit

--- day64/classwork/lesson0.py
+++ day64/classwork/lesson0.py
@@ -23,3 +23,29 @@ def xor(a,b):
 # 5
 def get_real_floor(n):
     return n - 2 if n >= 13 else 0 if n == 0 else n - 1 if n > 0 else n
+
+# 6
+def goose_filter(birds):
+    geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+    return [i for i in birds if i not in geese]
+
+# 7
+def divisible_by(numbers, divisor):
+    return [i for i in numbers if i % divisor == 0]
+
+# 8
+def name_shuffler(str_):
+    return " ".join(str_.split()[::-1])
+
+# 9
+def pipe_fix(nums):
+    return [i for i in range(min(nums), max(nums) + 1)]
+
+# 10
+def sale_hotdogs(n):
+    if n < 5:
+         return n * 100
+    elif 5 and n < 10:
+        return n * 95
+    else:
+        return n * 90
