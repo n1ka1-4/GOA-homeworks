7e49c786c73162b8a49d3ecc0e9adef5a769b26f
n1ka1-4 <nikoloz1tefnadze10.19@gmail.com>
2025-05-28 21:31:31 +0400

    commit

--- day102/classwork/script.js
+++ day102/classwork/script.js
@@ -1,3 +1,11 @@
 const conver = document.querySelector(".btn")
 const rate = document.querySelector("#ntr")
 const amount = document.querySelector("#mrt")
+const output = document.querySelector("#output")
+
+
+const intrest = rate.value / 100
+
+conver.addEventListener("click", (intrest) => {
+    output.textContent = `${intrest * amount.value}`
+})
